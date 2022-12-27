3ba815adec0d62848b0ae396d4c181f7d41ae9d4
ReaToi <reatoi@mail.ru>
2022-12-28 00:56:22 +0600

    home work 7

--- 25-2_beksultan_nurbekov_hw_7.py
+++ 25-2_beksultan_nurbekov_hw_7.py
@@ -1,42 +1,57 @@
-import random
+from random import randint, choice
 
 k = []
-for i in range(11):
-    k.append(random.randint(0, 100))
+for i in range(12):
+    k.append(randint(0, 100))
 print(f'unsorted list: {k}')
 def buble_sort(ls:list):
     for i in range(len(ls) - 1):
-        for i in range(len(ls) - 1):
-            if ls[i] > ls[i+1]:
-                ls[i],ls[i+1] = ls[i+1], ls[i]
+        for j in range(len(ls) - 1 - i):
+            if ls[j] > ls[j+1]:
+                ls[j],ls[j+1] = ls[j+1], ls[j]
     return ls
-value = random.choice(buble_sort(k))
+value = choice(buble_sort(k))
 print(f'desired number: {value}')
 print(f'sorted list: {k}')
 def bynary_search(value: int, ls: list):
-    result = False
-    first = len(ls) - len(ls)
+    # result = False
+    first = 0
     last = len(ls)
     # print(last)
     while True:
         if first < last:
             middle = (first + last) // 2
             if value == ls[middle]:
+                print('i found the number i was looking for')
                 first = middle
                 last = first
-                result = True
-                print('i found the number i was looking for')
+                pos = middle
+                print(f'position:{pos}')
                 break
             elif value > ls[middle]:
                 first = middle + 1
             elif value < ls[middle]:
                 last = middle - 1
-        elif result == True:
-            print('i found the number i was looking for')
-            break
         else:
             print('element not found')
             break
 
 bynary_search(value, k)
+
+
+
+unsorted_list = []
+for i in range(11):
+    unsorted_list.append(randint(1, 99))
+
+def selection_sort(ls: list):
+    for i in range(len(ls)):
+        min_val = ls[i]
+        for j in range(len(ls) - i):
+            if i < min_val:
+                min_val = i
+        ls[i], ls[min_val] = ls[min_val], ls[i]
+    return ls
+print(f'\nunsorted list:{unsorted_list}')
+print(f'sorted (list selection sort):{selection_sort(unsorted_list)}')
 
